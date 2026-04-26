a035f12659a740b9a01787e9d613e428d22b6297
GabiPondian <pondiangabi@gmail.com>
2026-04-25 22:18:41 -0300

    video 3 - CardList

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CardList from './Components/CardList/CardList';
+import CardList from './Components/CardList/CardList'
 
 function App() {
   return (
