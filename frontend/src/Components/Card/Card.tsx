dca60d4537d971f1448986877b6a8036f35a3a11
GabiPondian <pondiangabi@gmail.com>
2026-04-25 22:12:28 -0300

    remove submodule e adiciona frontend corretamente

---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Card.css";
+
+interface Props {
+    companyName: string;
+    ticker: string;
+    price: number;
+    
+}
+
+const Card = ({companyName, ticker, price}: Props) => {
+    return (
+        <div className='card'>
+            <img src="https://source.unsplash.com/random" alt="Imagem aleatória"/>
+       
+        <div className='details'>
+            <h2>{companyName} ({ticker})</h2>
+            <p>${price}</p>
+        </div>
+        <p className='info'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, perferendis.</p>
+
+        </div>
+    )
+}
+
+export default Card
