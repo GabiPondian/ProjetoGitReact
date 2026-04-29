bdfe0bbbf29776313e5a4de02619bfbce1002754
GabiPondian <pondiangabi@gmail.com>
2026-04-28 23:33:15 -0300

    video 11 - list

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,24 +1,23 @@
 import React, { JSX } from 'react'
 import "./Card.css";
+import { CompanySearch } from '../../company';
 
 interface Props {
-    companyName: string;
-    ticker: string;
-    price: number;
+    id: string;
+    searchResult: CompanySearch;
     
 }
 
-const Card: React.FC<Props> = ({companyName, ticker, price}: Props) : JSX.Element=> {
-    
+const Card: React.FC<Props> = ({id, searchResult}: Props) : JSX.Element=> {
     return (
         <div className='card'>
-            <img src="https://source.unsplash.com/random" alt="Imagem aleatória"/>
+            <img alt="Company logo"/>
        
         <div className='details'>
-            <h2>{companyName} ({ticker})</h2>
-            <p>${price}</p>
+            <h2>{searchResult.name} ({searchResult.symbol})</h2>
+            <p>${searchResult.currency}</p>
         </div>
-        <p className='info'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde, perferendis.</p>
+        <p className='info'>{searchResult.exchangeShortName} - {searchResult.stockExchange}</p>
 
         </div>
     )
